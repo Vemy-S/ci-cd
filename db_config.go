78d2078d620a2baf688f62cda988f5ed952d30ad
Jeremy Vega <jeredev20@gmail.com>
2026-05-28 19:11:36 -0400

    mongodb

--- db_config.go
+++ db_config.go
@@ -5,22 +5,24 @@ import (
 	"fmt"
 	"os"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 func ConnectDB() (*mongo.Client, error) {
-
 	uri := os.Getenv("MONGO_URL")
 	dbName := os.Getenv("NAME_DB")
 
-	clientOptions := options.Client().ApplyURI(uri)
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(opts)
 	if err != nil {
 		return nil, err
 	}
 
-	err = client.Ping(context.TODO(), nil)
+	err = client.Ping(context.TODO(), readpref.Primary())
 	if err != nil {
 		return nil, err
 	}
